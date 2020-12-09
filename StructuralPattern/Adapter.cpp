cad85447bbc6962b69fa97e8c3ec886fee8c4b82
zhuxiaoxuhit <zhuxiaoxuhit@gmail.com>
2020-12-09 11:21:17 +0800

    [MDF] Adapter

--- StructuralPattern/Adapter.cpp
+++ StructuralPattern/Adapter.cpp
@@ -2,27 +2,26 @@
 
 class Target{
 	public:
-		virtual void operation1() = 0;
-		virtual void operation2() = 0;
+		virtual void operation1() {std::cout << "Operation 1" << std::endl;}
+		virtual void operation2() {std::cout << "Operation 2" << std::endl;}
 };
 
 class Adaptee{
 	public:
-		virtual void specialOperation() {std::cout << "Special Operation" << std::endl;}
+		void specialoperation() {std::cout << "Special Operation" << std::endl;}
 };
 
 class Adapter : public Target{
 	public:
-		virtual void operation1() override {std::cout << "Operation1" << std::endl;}
-		virtual void operation2() override {adaptee_.specialOperation();}
+		virtual void operation2() override {adaptee_.specialoperation();}
 	private:
 		Adaptee adaptee_;
 };
 
 int main(){
-	Adapter a;
-	a.operation1();
-	a.operation2();
+	Adapter adapter;
+	adapter.operation1();
+	adapter.operation2();
+	return 0;
 }
-
 
